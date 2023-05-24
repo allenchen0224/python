eae33c2d72fd538954c98d759a8066e376923f8b
allen <x685221z@gmail.com>
2023-05-24 18:10:01 +0800

    檔案匯入

---
+++ file.py
@@ -0,0 +1,20 @@
+#儲存檔案
+# file = open('data.txt',mode = 'w',encoding='utf8')
+# file.write('測試中文\n好棒棒')
+# file.close()
+
+# with open ('data.txt',mode='w',encoding='utf8') as file:
+#     file.write('5\n3')
+#讀取檔案
+# sum = 0
+# with open ('data.txt',mode = 'r',encoding='utf8') as file:
+#     for line in file:
+#         sum+=int(line)
+# print(sum)
+#使用 JSON 格式讀取、複寫檔案
+import json
+with open('config.json',mode='r',encoding='big5') as file:
+    data = json.load(file)
+print(data)
+# print('name:',data['name'])
+# print('version:',data['version'])
