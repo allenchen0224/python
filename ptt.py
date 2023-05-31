ab6e47419ee54ba7a1b0a824d8d27ae35458a29d
allenchen0224 <133838297+allenchen0224@users.noreply.github.com>
2023-05-31 16:59:30 +0800

    Update ptt.py

--- ptt.py
+++ ptt.py
@@ -1,3 +1,5 @@
+import datetime
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 html = requests.get('https://www.ptt.cc/bbs/hotboards.html')
@@ -6,69 +8,41 @@ list = []
 a = soup.find_all('div',class_='board-name')
 for i in a:
     list.append(i.text)
-# print(list)
-# https://www.ptt.cc/bbs/Gossiping/index.html
-# https://www.ptt.cc/bbs/hotboards.html
 
 b= input('請輸入欲查詢的分類：')
 if b in list:
     b = b
 else:
     b = input('請重新輸入欲查詢的分類：')
+    
+r = requests.get('https://www.ptt.cc/bbs/'+b+'/index.html',headers={'cookie':'over18=1'})
+soup1 = BeautifulSoup(r.text,'html.parser')
+btn = soup1.select('div.btn-group >a')
+next_page = btn[3]['href']
+new = int(btn[3]['href'].split("index")[-1].split(".html")[0].strip())+1
 
+n = eval(input('請輸入欲查詢的頁數:'))
+end = new-n
 c=[]
 d=[]
 def search(content):
-    content = requests.get('https://www.ptt.cc/bbs/'+b+'/index.html',headers={'cookie':'over18=1'})
+    for page in range(new,end,-1):
+        content = requests.get('https://www.ptt.cc/bbs/'+b+'/index'+str(page)+'.html',headers={'cookie':'over18=1'})
+    
+        soup = BeautifulSoup(content.text,'html.parser')
+        title = soup.find_all('div',class_='title')
+        for i in title:
+            c.append(i.text.strip('\n'))
+        print(c)
+        link = soup.find_all('div',class_='title')
+        for j in link:
+            if j.select_one('a').get('href') != '':
+                d.append('https://www.ptt.cc/'+j.select_one("a").get("href"))
+            else:
+                d.append('')
+        print(d)
+search('content')
 
-    soup = BeautifulSoup(content.text,'html.parser')
-    # ment = soup.find_all("div",class_="m-ent")
-    # #一個一個印出要的資料
-    # for title in ment:
-    #     print(title.a.string)#取得文章標題
-    #     print("https://www.ptt.cc"+title.a.get("href"))#取得文章連結
-    title = soup.find_all('div',class_='title')
-    for i in title:
-        c.append(i.text.strip('\n'))
-    print(c)
-    link = soup.find_all('div',class_='title')
-    for j in link:
-        d.append('https://www.ptt.cc/'+j.select_one("a").get("href"))
-    print(d)
-
-def more(content):
-    content = requests.get('https://www.ptt.cc/bbs/'+b+'/index'+url+'.html',headers={'cookie':'over18=1'})
-
-    soup = BeautifulSoup(content.text,'html.parser')
-    # ment = soup.find_all("div",class_="m-ent")
-    # #一個一個印出要的資料
-    # for title in ment:
-    #     print(title.a.string)#取得文章標題
-    #     print("https://www.ptt.cc"+title.a.get("href"))#取得文章連結
-    title = soup.find_all('div',class_='title')
-    for i in title:
-        c.append(i.text.strip('\n'))
-    print(c)
-    link = soup.find_all('div',class_='title')
-    for j in link:
-        d.append('https://www.ptt.cc/'+j.select_one("a").get("href"))
-    print(d)
-
-n = eval(input('請輸入欲查詢的頁數:'))
-for page in range(1,n+1):
-    r = requests.get('https://www.ptt.cc/bbs/'+b+'/index.html',headers={'cookie':'over18=1'})
-    soup = BeautifulSoup(r.text,'html.parser')
-    btn = soup.select('div.btn-group >a')
-    next_page = btn[3]['href']
-    next_page_url = 'https://www.ptt.cc' + next_page
-    url = next_page_url
-    if page ==1:
-        search('content')
-    else:
-        more('content')
-
-    # btn = soup.select('div.btn-group > a')
-    # up_page_href = btn[3]['href']
-    # next_page_url = 'https://www.ptt.cc' + up_page_href
-    # url = next_page_url
-    # search('content')
+dict1 = {'title':c,'link':d}
+df = pd.DataFrame(dict1)
+df.to_csv('ppt.csv',encoding='utf-8-sig',index = False)
